cf54bc0dd4760ce10eda9b78faf8bdb3b74377ac
https://github.com/nyela/site1.git <https://github.com/nyela/site1.git>
2019-12-22 20:34:22 -0300

    python

---
+++ 01.py
@@ -0,0 +1,48 @@
+# Na linguagem de py não precisa informar a variável.
+#Para saber qual o tipo de variável usamos o type. exemplo: print(type(soma))
+# O input é para aparecer uma  mensagem para você informar o calor.exemplo: a = input ('Informe um valor')
+
+a= int (input ('Informe o primeiro valor, por favor:'))
+b= int (input ('informe o segundo valor, por favor:'))
+soma = a +b
+print (soma)
+
+#soma
+
+#a= 200
+#b=100
+#soma= a+b
+#print ('soma: ' , soma)
+#print (type (soma))
+
+#Como se faz uma subtração?
+#converter uma variável inteiro para string é so usar "STR". Exemplo: subtracao = str(subtracao).
+
+#a= 200
+#b=100
+#subtracao= a -b
+#subtracao = str(subtracao)
+#print(type(subtracao))
+
+#Como se faz uma multiplicação?
+#concatenação:"print('multiplicação: {}' .format(multi))",Caso queira usar uma soma na mesmo print
+#é usar por exemplo:"print('multiplicação: {} \ndivi:{}' .format(multi, divi))", entre parentes posso usar nome.
+#o barra n é como se posse o enter: Exemplo: \n.
+#a= 200
+#b=100
+#multi = a * b
+#print('multiplicação: {}' .format(multi))
+
+#Como se faz uma divisão?
+
+#a=200
+#b=100
+#divi=a/b
+#print(divi)
+
+#Como se calcular para mostrar o resto?
+
+#a=200
+#b=100
+#resto=a%b
+#print(resto)
